16ac98e402b9c86f80e8d87a824ba2b18b41867c
GasanovShamil <gasanov.sh@gmail.com>
2017-06-07 18:35:18 +0200

    admin role, tokens, navbar

---
+++ app/controllers/custom_sessions_controller.rb
@@ -0,0 +1,10 @@
+class CustomSessionsController < Devise::SessionsController
+  before_filter :before_login, :only => :create
+  after_filter :after_login, :only => :create
+
+  def before_login
+  end
+
+  def after_login
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   protected
+
+   def serializable_hash(options = nil) 
+    super(options).merge(last_sign_in_at: last_sign_in_at, current_sign_in_at: current_sign_in_at) # you can keep adding attributes here that you wish to expose
+  end
 end
