a9641897d3c3e6dadca17c9fda6c704a5a455e59
GasanovShamil <gasanov.sh@gmail.com>
2017-06-09 09:22:24 +0200

    rooms controller and views WITHOUT model for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  resources 'rooms'
  # devise_for :users
   devise_for :users, :controllers => { :sessions => "custom_sessions" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
