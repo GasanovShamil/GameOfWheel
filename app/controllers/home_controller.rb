e6b878e3ae61d20f6d93d04dbf37aff6c99fc645
Léo Foltz <leo.foltzrahem@gmail0com>
2017-06-28 15:10:14 +0200

    now watch me whip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
 	before_action :authenticate_user!, only: [:index]	
 	def index
+		@all_rooms = Room.order("RANDOM()").limit(3)
+		@my_rooms = Room.order("RANDOM()").limit(3)
 	end
 end
