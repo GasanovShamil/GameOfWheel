433da51533152e8dbd049e68f197e15a2d663af0
GasanovShamil <gasanov.sh@gmail.com>
2017-07-13 01:04:38 +0200

    heroku fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,7 @@ class HomeController < ApplicationController
 		@all_rooms = Room.where('start_date <= ? AND end_date >= ?', DateTime.now, DateTime.now).order("RANDOM()").limit(3)
 
 		if user_signed_in?
-			@my_rooms = current_user.rooms.limit(3).uniq.order("RANDOM()")
+			@my_rooms = current_user.rooms.select('rooms.*').limit(3).order("RANDOM()").uniq
 		end
 	end
 end
-
